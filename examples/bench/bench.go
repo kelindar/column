5d626ecba688715108c70efb37d3de0a25a5c048
Roman Atachiants <roman.atachiants@gmail.com>
2021-09-11 02:39:49 +0400

    Fixed point read latch

--- examples/bench/bench.go
+++ examples/bench/bench.go
@@ -7,7 +7,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"hash/crc32"
+	"math/bits"
 	"os"
 	"sync"
 	"sync/atomic"
@@ -31,13 +31,13 @@ func main() {
 	createCollection(players, amount)
 
 	// This runs point query benchmarks
-	runBenchmark("Point Reads/Writes", func(writeTxn bool) (reads int, writes int) {
+	runBenchmark("Point Reads/Writes", func(v uint32, writeTxn bool) (reads int, writes int) {
 
 		// To avoid task granuarity problem, load up a bit more work on each
 		// of the goroutines, a few hundred reads should be enough to amortize
 		// the cost of scheduling goroutines, so we can actually test our code.
 		for i := 0; i < 1000; i++ {
-			offset := randN(amount - 1)
+			offset := randN(v, amount-1)
 			if writeTxn {
 				players.UpdateAt(offset, "balance", func(v column.Cursor) error {
 					v.SetFloat64(0)
@@ -56,7 +56,7 @@ func main() {
 }
 
 // runBenchmark runs a benchmark
-func runBenchmark(name string, fn func(bool) (int, int)) {
+func runBenchmark(name string, fn func(uint32, bool) (int, int)) {
 	fmt.Printf("Benchmarking %v ...\n", name)
 	fmt.Printf("%7v\t%6v\t%17v\t%13v\n", "WORK", "PROCS", "READ RATE", "WRITE RATE")
 	for _, workload := range []int{0, 10, 50, 90, 100} {
@@ -69,12 +69,12 @@ func runBenchmark(name string, fn func(bool) (int, int)) {
 			var reads, writes int64
 			var wg sync.WaitGroup
 			start := time.Now()
-			for time.Since(start) < time.Second {
+			for i := uint32(0); time.Since(start) < time.Second; i++ {
 				wg.Add(1)
 				work <- async.NewTask(func(ctx context.Context) (interface{}, error) {
 					defer wg.Done()
 
-					r, w := fn(chanceOf(workload))
+					r, w := fn(i, chanceOf(i, workload))
 					atomic.AddInt64(&reads, int64(r))
 					atomic.AddInt64(&writes, int64(w))
 					return nil, nil
@@ -145,21 +145,24 @@ func createCollection(out *column.Collection, amount int) *column.Collection {
 	return out
 }
 
-var epoch uint32
-
 // This random number generator not the most amazing one, but much better
 // than using math.Rand for our benchmarks, since it would create a lock
 // contention and bias the results.
-func randN(n int) uint32 {
-	v := atomic.AddUint32(&epoch, 1)
-	return crc32.ChecksumIEEE([]byte{
-		byte(v >> 24),
-		byte(v >> 16),
-		byte(v >> 8),
-		byte(v),
-	}) % uint32(n)
+func randN(v uint32, n int) uint32 {
+	return uint32(xxhash(v) % uint64(n))
 }
 
-func chanceOf(chance int) bool {
-	return randN(100) < uint32(chance)
+func chanceOf(v uint32, chance int) bool {
+	return randN(v, 100) < uint32(chance)
+}
+
+func xxhash(v uint32) uint64 {
+	packed := uint64(v) + uint64(v)<<32
+	x := packed ^ (0x1cad21f72c81017c ^ 0xdb979083e96dd4de)
+	x ^= bits.RotateLeft64(x, 49) ^ bits.RotateLeft64(x, 24)
+	x *= 0x9fb21c651e98df25
+	x ^= (x >> 35) + 4 // len
+	x *= 0x9fb21c651e98df25
+	x ^= (x >> 28)
+	return x
 }
