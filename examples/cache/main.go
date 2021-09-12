369cfdb63303eb619c0a436410e25980656699ef
Roman Atachiants <roman.atachiants@gmail.com>
2021-09-13 01:08:43 +0400

    Added kv example

---
+++ examples/cache/main.go
@@ -0,0 +1,56 @@
+// Copyright (c) Roman Atachiants and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/kelindar/xxrand"
+)
+
+func main() {
+	amount := 50000
+	cache := New()
+
+	fmt.Println("inserting users...")
+	for i := 0; i < amount; i++ {
+		key := fmt.Sprintf("user_%d", i)
+		val := fmt.Sprintf("Hi, User %d", i)
+		cache.Set(key, val)
+
+		if (i+1)%10000 == 0 {
+			fmt.Printf("-> inserted %v rows\n", i+1)
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		key := fmt.Sprintf("user_%d", xxrand.Intn(amount))
+		measure("query", key, func() {
+			fmt.Println(cache.Get(key))
+		}, 10000)
+	}
+}
+
+func measure(action, name string, fn func(), iterations int) {
+	defer func(start time.Time, stdout *os.File) {
+		os.Stdout = stdout
+		elapsed := time.Since(start) / time.Duration(iterations)
+		fmt.Printf("-> %v took %v\n", action, elapsed.String())
+	}(time.Now(), os.Stdout)
+
+	fmt.Println()
+	fmt.Printf("running %v of %v...\n", action, name)
+
+	// Run a few times so the results are more stable
+	null, _ := os.Open(os.DevNull)
+	for i := 0; i < iterations; i++ {
+		if i > 0 { // Silence subsequent runs
+			os.Stdout = null
+		}
+
+		fn()
+	}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/kelindar/column
 go 1.17
 
 require (
-	github.com/dustin/go-humanize v1.0.0
-	github.com/kelindar/async v1.0.0
 	github.com/kelindar/bitmap v1.1.1
 	github.com/kelindar/smutex v1.0.0
 	github.com/stretchr/testify v1.7.0
@@ -12,6 +10,9 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0
+	github.com/kelindar/async v1.0.0
+	github.com/kelindar/xxrand v1.0.1
 	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
