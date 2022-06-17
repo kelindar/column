a7f8e39dcba4084ae7efa8999ce5b390c716ddef
William Dreese <wdreese123@gmail.com>
2022-06-16 21:22:45 -0400

    adding test

--- txn_test.go
+++ txn_test.go
@@ -726,6 +726,41 @@ func TestUnion(t *testing.T) {
 	})
 }
 
+func TestMultiUnion(t *testing.T) {
+	c := NewCollection()
+	c.CreateColumn("tester", ForString())
+	c.CreateColumn("testerB", ForString())
+
+	c.CreateIndex("tester_1", "tester", func(r Reader) bool { return r.String() == "1" })
+	c.CreateIndex("tester_2", "tester", func(r Reader) bool { return r.String() == "2" })
+	c.CreateIndex("tester_3", "tester", func(r Reader) bool { return r.String() == "3" })
+	c.CreateIndex("testerB_1", "tester", func(r Reader) bool { return r.String() == "4" })
+	c.CreateIndex("testerB_2", "tester", func(r Reader) bool { return r.String() == "5" })
+	c.CreateIndex("testerB_3", "tester", func(r Reader) bool { return r.String() == "6" })
+
+	c.InsertObject(map[string]interface{}{
+		"tester": "1",
+		"testerB": "4",
+	})
+	c.InsertObject(map[string]interface{}{
+		"tester": "2",
+		"testerB": "5",
+	})
+	c.InsertObject(map[string]interface{}{
+		"tester": "3",
+		"testerB": "6",
+	})
+
+	c.Query(func(txn *Txn) error {
+		// where tester in ['1', '2'] and testerB in ['4', '5']
+		txn = txn.Union("tester_1", "tester_2")
+		txn = txn.Union("testerB_4", "testerB_5")
+
+		assert.Equal(t, 1, txn.Count())
+		return nil
+	})
+}
+
 func TestSumBalance(t *testing.T) {
 	players := loadPlayers(500)
 	assert.Equal(t, 500, players.Count())
